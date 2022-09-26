7404a91ba334f8941c43580798f2cc477914e694
Theobirmp <theobirmpil@gmail.com>
2022-09-26 15:13:03 +0300

    end game states done, both can burn

--- features/cardsSlice.js
+++ features/cardsSlice.js
@@ -5,7 +5,7 @@ const emojis = require('./data/emojis')
  const initialState={
     cards:[
         //spades
-        {number:1,sign:'spades',emoji:emojis.spades,value:1},
+        {number:1,sign:'spades',emoji:emojis.spades,value:11},
         {number:2,sign:'spades',emoji:emojis.spades,value:2},
         {number:3,sign:'spades',emoji:emojis.spades,value:3},
         {number:4,sign:'spades',emoji:emojis.spades,value:4},
@@ -16,7 +16,7 @@ const emojis = require('./data/emojis')
         {number:9,sign:'spades',emoji:emojis.spades,value:9},
         {number:10,sign:'spades',emoji:emojis.spades,value:10},
         //hearts
-        {number:1,sign:'hearts',emoji:emojis.hearts,value:1},
+        {number:1,sign:'hearts',emoji:emojis.hearts,value:11},
         {number:2,sign:'hearts',emoji:emojis.hearts,value:2},
         {number:3,sign:'hearts',emoji:emojis.hearts,value:3},
         {number:4,sign:'hearts',emoji:emojis.hearts,value:4},
@@ -27,7 +27,7 @@ const emojis = require('./data/emojis')
         {number:9,sign:'hearts',emoji:emojis.hearts,value:9},
         {number:10,sign:'hearts',emoji:emojis.hearts,value:10},
         //clubs
-        {number:1,sign:'clubs',emoji:emojis.clubs,value:1},
+        {number:1,sign:'clubs',emoji:emojis.clubs,value:11},
         {number:2,sign:'clubs',emoji:emojis.clubs,value:2},
         {number:3,sign:'clubs',emoji:emojis.clubs,value:3},
         {number:4,sign:'clubs',emoji:emojis.clubs,value:4},
@@ -38,7 +38,7 @@ const emojis = require('./data/emojis')
         {number:9,sign:'clubs',emoji:emojis.clubs,value:9},
         {number:10,sign:'clubs',emoji:emojis.clubs,value:10},
         //diamonds
-        {number:1,sign:'diamonds',emoji:emojis.diamonds,value:1},
+        {number:1,sign:'diamonds',emoji:emojis.diamonds,value:11},
         {number:2,sign:'diamonds',emoji:emojis.diamonds,value:2},
         {number:3,sign:'diamonds',emoji:emojis.diamonds,value:3},
         {number:4,sign:'diamonds',emoji:emojis.diamonds,value:4},
@@ -53,7 +53,11 @@ const emojis = require('./data/emojis')
     dealerCards:[],
     playerPoints:0,
     dealerPoints:0,
+    playerHasAce:false,
+    dealerHasAce:false,
     selectedCard:null,
+    dealerBurnt:false,
+    playerBurnt:false
 }
 
 const cardsSlice= createSlice({
@@ -79,40 +83,40 @@ const cardsSlice= createSlice({
             state.playerCards=[]
             state.dealerCards=[]
         },
-        // showPlayerCards:(state)=>{
-        //     console.log('Your Cards')
-        //     let output=''
-        //     state.playerCards?.forEach(card=>{
-        //         output+=`${card.number}${card.emoji} `
-        //     })
-        //     console.log(output)
-        // },
-        showDealerCardsStart:(state)=>{
-            console.log('Dealer Cards')
-            let output=`${state.dealerCards[0].number}${state.dealerCards[0].emoji} x?`
-            console.log(output)
-        },
-        // showDealerCards:(state)=>{
-        //     console.log('Dealer Cards')
-        //     let output=''
-        //     state.dealerCards?.forEach(card=>{
-        //         output+=`${card.number}${card.emoji} `
-        //     })
-        //     console.log(output)
-        // },
         getPlayerPoints:(state)=>{
             state.playerPoints=state.playerCards.reduce((points,item)=>{
                 return points=points+item.value
             },0)
+            //check if player can be saved by ace
+            if(state.playerPoints>21){
+                state.playerHasAce=state.playerCards.some(card=>card.value==11)
+                if(state.playerHasAce) state.playerPoints=state.playerPoints-10
+            }
         },
         getDealerPoints:(state)=>{
             state.dealerPoints=state.dealerCards.reduce((points,item)=>{
                 return points=points+item.value
             },0)
+            if(state.dealerPoints>21){
+                state.dealerHasAce=state.dealerCards.some(card=>card.value==11)
+                if(state.dealerHasAce) state.dealerPoints=state.dealerPoints-10
+            }
+        },
+        playerHasAce:(state,action)=>{
+            if(action.payload==true) state.playerPoints=state.playerPoints-10
+        },
+        dealerHasAce:(state,action)=>{
+            if(action.payload==true) state.dealerPoints=state.dealerPoints-10
+        },
+        playerBurnt:(state)=>{
+            state.playerBurnt=true
+        },
+        dealerBurnt:(state)=>{
+            state.dealerBurnt=true
         },
         //helpers
         getCards:(state)=>{
-            state.cards.forEach(card=>console.log(card.number,card.sign))
+             state.cards.forEach(card=>console.log(card.number,card.sign))
         },
         getPlayerCards:(state)=>{
             console.log('player cards')
@@ -132,6 +136,7 @@ const cardsSlice= createSlice({
         }
     }
 })
+
 
 module.exports = cardsSlice
 module.exports.cardsReducer = cardsSlice.reducer
--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-// const initialState = require('./features/cardsSlice')
 const store = require('./app/store')
 //all actions
 const cardsActions=require('./features/cardsSlice').actions
@@ -13,7 +12,7 @@ const game=async()=>{
     const dealerPlays=await doesPlayerDrawCard()
     showDealerCards()
     if(!dealerPlays) return
-    const checkWin = await doesDealerDrawCard()
+    const checkWin =  doesDealerDrawCard()
     if(!checkWin) return
     checkWinner()
 
@@ -37,6 +36,10 @@ const startGame=()=>{
     showDealerCardsStart()
 }
 const endGame=(action)=>{
+    const {cardsReducer:{playerBurnt,dealerBurnt}}=store.getState()
+    if(playerBurnt || dealerBurnt) {
+        return
+    }
     switch(action){
         case gameEndStates.DEALER_BURNT:
             console.log('Dealer burnt - You won')
@@ -63,21 +66,21 @@ const playerDrewCard=()=>{
     showDealerCardsStart()
 }
 const hasPlayerBurnt=()=>{
-    const {cardsReducer:{playerPoints}}=store.getState()
-    if(playerPoints>21){
-        endGame(gameEndStates.PLAYER_BURNT)
-        return true
+    let {cardsReducer:{playerPoints,playerCards}}=store.getState()
+        if(playerPoints>21){
+            store.dispatch(cardsActions.playerBurnt())
+            endGame(gameEndStates.PLAYER_BURNT)
+            return true
     } else {
         return false
     }
 }
 const dealerHasBurnt=()=>{
-    const {cardsReducer:{dealerPoints}}=store.getState()
-    // console.log(dealerPoints)
-    // console.log('player points are:',dealerPoints)
-    if(dealerPoints>21){
-        endGame(gameEndStates.DEALER_BURNT)
-        return true
+    const {cardsReducer:{dealerPoints,dealerCards}}=store.getState()
+        if(dealerPoints>21){
+            store.dispatch(cardsActions.dealerBurnt())
+            console.log('Dealer burnt - You win')
+            return true
     } else {
         return false
     }
@@ -85,7 +88,7 @@ const dealerHasBurnt=()=>{
 const doesPlayerDrawCard=async()=>{
     let con=true
     while(con){
-        await inquirer.prompt([
+         await inquirer.prompt([
             {
                 type:'list',
                 message:'Draw Card',
@@ -112,52 +115,51 @@ const doesPlayerDrawCard=async()=>{
             }
           });
     }
-    if(hasPlayerBurnt()) return false
+    if(hasPlayerBurnt()) {
+        store.dispatch(cardsActions.playerBurnt())
+        console.log('You burnt - Dealer wins')
+        return false
+    }
     return true
 
 }
 //checks if the dealer wants to draw a card
-const dealerThinks=async()=>{
+const dealerThinks=()=>{
     store.dispatch(cardsActions.getDealerPoints())
     const {cardsReducer:{dealerPoints}} = store.getState()
     console.log('Dealer Thinking...')
-    await sleep(2000)
+    // await sleep(2000)
     switch (dealerPoints) {
         case 21:
         case 20:
         case 19:
         case 18:
-            return false            
         case 17:
-            return randomChance(15)
+            return false
         case 16:
-            return randomChance(35)
-        case 15:
-            return randomChance(65)
-        case 14:
-            return randomChance(90)
-        case 13:
-            return true    
+            return true
         default: 
             return true
     }
 }
 
 const doesDealerDrawCard=async()=>{
-    let con=await dealerThinks()
+    let con= dealerThinks()
     while(con===true){
         console.log('Dealer Draws')
         store.dispatch(cardsActions.pickACard())
         store.dispatch(cardsActions.dealerDrawCard())
+        store.dispatch(cardsActions.getDealerPoints())
         showDealerCards()
         if(dealerHasBurnt())return false
-        con=await dealerThinks()
+        con= dealerThinks()
     }
     console.log('I dont want to draw more cards')
     showPlayerCards()
     showDealerCards()
     return true
 }
+
 const checkWinner=()=>{
     const {cardsReducer:{playerPoints}}=store.getState()
     const {cardsReducer:{dealerPoints}}=store.getState()
@@ -166,27 +168,22 @@ const checkWinner=()=>{
     if(playerPoints===dealerPoints) endGame(gameEndStates.DRAW)
 }
 const showPlayerCards=()=>{
-    const {cardsReducer:{playerCards}}=store.getState()
-    let totalPoints=playerCards.reduce((points,item)=>{
-        return points=points+item.value
-    },0)
+    const {cardsReducer:{playerCards,playerPoints}}=store.getState()
     let output='You: ' 
     playerCards.forEach(card=>{
         output+=`${card.number}${card.emoji} `
     })
-    output+='==> ' + totalPoints
+    output+='==> ' + playerPoints
     console.log(output)
 }
+
 const showDealerCards=()=>{
-    const {cardsReducer:{dealerCards}}=store.getState()
-    let totalPoints=dealerCards.reduce((points,item)=>{
-       return points=points+item.value
-    },0)
+    const {cardsReducer:{dealerCards,dealerPoints}}=store.getState()
     let output='Dealer: ' 
     dealerCards.forEach(card=>{
         output+=`${card.number}${card.emoji} `
     })
-    output+='==> ' + totalPoints
+    output+='==> ' + dealerPoints
     console.log(output)
 }
 const showDealerCardsStart=()=>{
@@ -196,6 +193,9 @@ const showDealerCardsStart=()=>{
     },0)
     let output=`Dealer: ${dealerCards[0].number}${dealerCards[0].emoji} x? ==> ${dealerCards[0].value}`
     console.log(output)
+}
+const checkForAces=(cards)=>{
+    return cards.some(card=>card.number==1)
 }
 module.exports = game
 module.exports.startGame=startGame
