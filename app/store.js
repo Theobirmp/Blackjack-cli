81b7148a0116ddd7a048cd54a2a19c3b3185122d
Theobirmp <theobirmpil@gmail.com>
2022-09-30 01:46:19 +0300

    fixed import bug, added picture cards

--- app/store.js
+++ app/store.js
@@ -2,15 +2,11 @@
 //configure store
 const configureStore = require('@reduxjs/toolkit').configureStore
 //all reducers
-const { dealerCardsReducer } = require('../features/dealerCardsSlice')
 const cardsReducer=require('../features/cardsSlice').cardsReducer
-const playerCardsReducer=require('../features/playerCardsSlice').playerCardsReducer
 
 const store=configureStore({
     reducer:{
         cardsReducer:cardsReducer,
-        playerCardsReducer:playerCardsReducer,
-        dealerCardsReducer:dealerCardsReducer,
     }
 })
 
